ea626e07acaf273e6b7460591bd59413f517e6c3
EshwarKG1 <101560313+EshwarKG1@users.noreply.github.com>
2022-03-16 10:34:41 +0530

    Update file_word.py

--- Basics/file_word.py
+++ Basics/file_word.py
@@ -1,16 +1,29 @@
-with open("myWord.txt","r") as f:
-    word = f.read()
-
-words = word.split(",")
-
 with open("dict.txt","r") as file:
     ele = file.read()
-
 key = ele.split(",")
+
+with open("myWord.txt","r") as f:
+    for line in f:
+        for word in line.split():
+            if word in key:
+                pass
+            else:
+                print(word,"is not present in the dict file")
+                flag = int(input("Select 1 for Ignore\nSelect 2 for add\n: "))
+                if flag == 2:
+                    with open("dict.txt","a") as f:
+                        f.write(",")
+                        f.write(word)
+                    print(word,"successfully added to dict file\n")
+                elif flag == 1:
+                    print(word,"has been ignored\n")
+
+"""
 
 for i in range(len(words)):
     if words[i] in key:
-        print(words[i]," is present")
+        #print(words[i]," is present")
+        pass
     else:
         print(words[i]," is not present")
         print("Select 1 to ignore")
@@ -23,7 +36,7 @@ for i in range(len(words)):
         elif flag == 1:
             print(words[i],"has been ignored")
 
-
+"""
 
 """
 if key in word:
