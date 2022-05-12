b524da063b8df50ad06f96b766d304748e2c2cb4
EshwarKG1 <eshwar.kg@acufore.com>
2022-05-12 17:52:59 +0530

    Updated 12-May

--- ESP32- programs/Wifi_codes/Wifi_codes/Wifi_codes.ino
+++ ESP32- programs/Wifi_codes/Wifi_codes/Wifi_codes.ino
@@ -27,7 +27,14 @@ void setup() {
   Serial.print("Got IP: ");
   Serial.println(WiFi.localIP());  //Show ESP32 IP on serial
 
-  server.on("/home", handle_root);
+  server.on("/", handle_root);
+  server.on("/mixture1", handle_mixture1);
+  server.on("/mixture2", handle_mixture2);
+  server.on("/mixture3", handle_mixture3);
+  server.on("/mixture4", handle_mixture4);
+  server.on("/mixture5", handle_mixture5);
+  server.on("/mixture6", handle_mixture6);
+  server.on("/mixture7", handle_mixture7);
 
   server.begin();
   Serial.println("HTTP server started");
@@ -38,6 +45,56 @@ void loop() {
   server.handleClient();
 }
 
+void handle_mixture1(){
+  server.send(200, "text/html", HTML);
+  dacCurve(0,255,0,10);
+}
+
+void handle_mixture2(){
+  server.send(200, "text/html", HTML);
+  dacCurve(0,180,0,10);
+}
+
+void handle_mixture3(){
+  server.send(200, "text/html", HTML);
+  dacCurve(0,125,0,10);
+}
+
+void handle_mixture4(){
+  server.send(200, "text/html", HTML);
+  dacCurve(0,80,0,10);
+}
+
+void handle_mixture5(){
+  server.send(200, "text/html", HTML);
+  dacCurve(0,125,0,10);
+}
+
+void handle_mixture6(){
+  server.send(200, "text/html", HTML);
+  dacCurve(0,180,0,10);
+}
+
+void handle_mixture7(){
+  server.send(200, "text/html", HTML);
+  dacCurve(0,255,0,10);
+}
+
+void dacCurve(int start_point,int peak_point,int end_point,int delay_time){
+  
+  for(int i = start_point; i < peak_point; i++){
+    dacWrite(DAC1,i);
+    dacWrite(DAC2,i);
+    delay(delay_time);
+  }
+  
+  for(int i = peak_point; i >= end_point; i--){
+    dacWrite(DAC1,i);
+    dacWrite(DAC2,i);
+    delay(delay_time);
+  }
+
+}
 
 // Handle root url (/)
 void handle_root() {
--- ESP32- programs/Wifi_codes/Wifi_codes/root_page.h
+++ ESP32- programs/Wifi_codes/Wifi_codes/root_page.h
@@ -1,6 +1,53 @@
 String HTML = "<!DOCTYPE html>\
 <html>\
+<head>\
+<title>PID Analyzer</title>\
+</head>\
 <body>\
-<h1>My First Web Server with ESP32 & Rpi- Station Mode &#128522;</h1>\
+<h1> PID Analyzer </h1>\
+<p>Please select any Gas Mixture option below</p>\
+<input type ='radio' id ='mixture1' name='gas_select' value='mixture1'>\
+<label for='mixture1'>vinyl chloride,Toluene</label><br>\
+<input type = 'radio' id ='mixture2' name='gas_select' value='mixture2'>\
+<label for = 'mixture2'>methylene chloride,Perchloroethylene</label><br>\
+<input type = 'radio' id ='mixture3' name='gas_select' value='mixture3'>\
+<label for='mixture3'>3 chloro propene,Chlorobenzene</label><br>\
+<input type = 'radio' id ='mixture4' name='gas_select' value='mixture4'>\
+<label for='mixture4'>Benzene, 1,2,4 trichloro benzene</label><br>\
+<input type = 'radio' id ='mixture5' name='gas_select' value='mixture5'>\
+<label for='mixture5'>vinyl chloride,cis 1,3 dichloropropene,Toluene</label><br>\
+<input type = 'radio' id ='mixture6' name='gas_select' value='mixture6'>\
+<label for = 'mixture6'>methylene chloride,trans 1,3 dichloropropene,Perchloroethylene</label><br>\
+<input type = 'radio' id ='mixture7' name='gas_select' value='mixture7'>\
+<label for='mixture7'>3 chloro propene,Toluene,Chlorobenzene</label><br><br>\
+<input type='submit' onclick=onStart() value='START'>\
+<p id='demo'></p>\
+<script>\
+function onStart() {\
+  var pageStr = '';\
+  if (document.getElementById('mixture1').checked){\
+    pageStr = 'mixture1';\
+  }\
+  else if (document.getElementById('mixture2').checked){\
+    pageStr = 'mixture2';\
+  }\
+  else if (document.getElementById('mixture3').checked){\
+    pageStr = 'mixture3';\
+  }\
+  else if (document.getElementById('mixture4').checked){\
+    pageStr = 'mixture4';\
+  }\
+  else if (document.getElementById('mixture5').checked){\
+    pageStr = 'mixture5';\
+  }\
+  else if (document.getElementById('mixture6').checked){\
+    pageStr = 'mixture6';\
+  }\
+  else if (document.getElementById('mixture7').checked){\
+    pageStr = 'mixture7';\
+  }\
+  window.location.href='/'+pageStr;\
+}\
+</script>\
 </body>\
-</html>";   
+</html>";
